9fdd3656cc9437b1674acf02ce130beef2d0a71c
Mateusz Rębacz <matirebacz@op.pl>
2018-04-22 00:48:41 +0200

    Added new block colors
    Block colors added: Black, Indigo, Orange and Red

--- src/main/java/com/logitow/logimine/blocks/ModBlocks.java
+++ src/main/java/com/logitow/logimine/blocks/ModBlocks.java
@@ -16,26 +16,29 @@ public class ModBlocks {
     //Below blocks will only appear as a part of a structures. They will not be available in creative.
     public static BlockBase white_lblock = new BlockBase(Material.CAKE,"white_lblock");
     public static BlockBase blue_lblock = new BlockBase(Material.CAKE,"blue_lblock");
-    public static BlockBase magenta_lblock = new BlockBase(Material.CAKE,"magenta_lblock");
+    public static BlockBase black_lblock = new BlockBase(Material.CAKE,"black_lblock");
     public static BlockBase pink_lblock = new BlockBase(Material.CAKE,"pink_lblock");
     public static BlockBase green_lblock = new BlockBase(Material.CAKE,"green_lblock");
     public static BlockBase purple_lblock = new BlockBase(Material.CAKE,"purple_lblock");
     public static BlockBase yellow_lblock = new BlockBase(Material.CAKE,"yellow_lblock");
+    public static BlockBase indigo_lblock = new BlockBase(Material.CAKE,"indigo_lblock");
+    public static BlockBase orange_lblock = new BlockBase(Material.CAKE,"orange_lblock");
+    public static BlockBase red_lblock = new BlockBase(Material.CAKE,"red_lblock");
     public static BlockBase end_lblock = new BlockBase(Material.CAKE,"end_lblock");
 
 
 
     public static void register(IForgeRegistry<Block> registry) {
         registry.registerAll(
-                key_lblock,white_lblock,blue_lblock,magenta_lblock,pink_lblock,green_lblock,purple_lblock,yellow_lblock, end_lblock
+                key_lblock,white_lblock,blue_lblock,black_lblock,pink_lblock,green_lblock,purple_lblock,yellow_lblock, indigo_lblock, orange_lblock, red_lblock, end_lblock
         );
 
     }
 
     public static void registerItemBlocks(IForgeRegistry<Item> registry) {
         registry.registerAll(
-                key_lblock.createItemBlock(),white_lblock.createItemBlock(),blue_lblock.createItemBlock(),magenta_lblock.createItemBlock(),
-                pink_lblock.createItemBlock(), green_lblock.createItemBlock(),purple_lblock.createItemBlock(),yellow_lblock.createItemBlock(), end_lblock.createItemBlock()
+                key_lblock.createItemBlock(),white_lblock.createItemBlock(),blue_lblock.createItemBlock(),black_lblock.createItemBlock(),
+                pink_lblock.createItemBlock(), green_lblock.createItemBlock(),purple_lblock.createItemBlock(),yellow_lblock.createItemBlock(), indigo_lblock.createItemBlock(), orange_lblock.createItemBlock(), red_lblock.createItemBlock(), end_lblock.createItemBlock()
         );
 
     }
@@ -44,11 +47,14 @@ public class ModBlocks {
         key_lblock.registerItemModel(Item.getItemFromBlock(key_lblock));
         white_lblock.registerItemModel(Item.getItemFromBlock(white_lblock));
         blue_lblock.registerItemModel(Item.getItemFromBlock(blue_lblock));
-        magenta_lblock.registerItemModel(Item.getItemFromBlock(magenta_lblock));
+        black_lblock.registerItemModel(Item.getItemFromBlock(black_lblock));
         pink_lblock.registerItemModel(Item.getItemFromBlock(pink_lblock));
         green_lblock.registerItemModel(Item.getItemFromBlock(green_lblock));
         purple_lblock.registerItemModel(Item.getItemFromBlock(purple_lblock));
         yellow_lblock.registerItemModel(Item.getItemFromBlock(yellow_lblock));
+        indigo_lblock.registerItemModel(Item.getItemFromBlock(indigo_lblock));
+        orange_lblock.registerItemModel(Item.getItemFromBlock(orange_lblock));
+        red_lblock.registerItemModel(Item.getItemFromBlock(red_lblock));
         end_lblock.registerItemModel(Item.getItemFromBlock(end_lblock));
     }
 }
